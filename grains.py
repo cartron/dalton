629871a6b8d61ddf09c70be918a75f2fd4887b97
cartron <cartermoar@gmail.com>
2013-08-12 22:26:34 -0700

    some color calculation stuff
    I'm not sure it's worth keeping

--- grains.py
+++ grains.py
@@ -17,6 +17,7 @@
 
 
 class Grain(object):
+#The basic class that all fermentables fall under
 
 	count = 0
 	
@@ -26,6 +27,7 @@ class Grain(object):
 			#This might need to live outside of the Grain class at some point (in a Recipe class 
 			#that aggregates grains and stuff (and does the total OG/color calculations?))
 		
+		#establish the main attributes
 		self.type = type
 		self.lb = lb
 		self.oz = oz
@@ -37,24 +39,36 @@ class Grain(object):
 		self.use = use
 		
 	def calculatePoints(self, volume, efficiency):
+		#returns the points added from a single fermentable
 		#this might not work for extract--look into that
 		pts = ((self.ppg * self.pounds) / volume) * efficiency
 		return pts
 			#I'm not certain that this is correct--might have to do some Qbrew testing
 
 	def returnOG(self, volume, efficiency):
+		#return the gravity contribution of a single fermentable addition
 		vol = volume
 		efficient = efficiency
 		pt = self.calculatePoints(vol, efficient)
 		OG = 1 + (pt / 1000)
 		return OG
-			
+	
+	def calculateColor(self):
+		#calculates the color contriution of a single fermentable addition
+		#the color from all additions should be divided by the total volume
+			#and fed through a color estimating formula (initially just Morey)
+		#this might not be the most effective way to do things
+		color = self.pounds * self.degreesL
+		return color
+		
+		
 		
 #test main
 grain = Grain(ppg=30)
 grainlb = Grain(oz=8)
 points = grain.calculatePoints(5, 0.75)
 og = grain.returnOG(5, 0.75)
+color = grain.calculateColor()
 
 print('type:', grain.type)
 print('weight:', grain.weight, 'oz')
@@ -63,4 +77,6 @@ print('points:', points)
 print('OG:', og)
 print('count:', grain.count)
 print('Count:', Grain.count)
+print('Color:',color)
+print('Color:',grainlb.calculateColor())
 input("\nExit")
