61c6f724b64d03be3e37fa53ea27104465f6943f
cartron <cartermoar@gmail.com>
2013-08-17 11:46:47 -0700

    some rudimentary notes

---
+++ water.py
@@ -0,0 +1,11 @@
+#	This will eventually perform most or all of the basic water calculations. It will hit a config file for user
+#	equipment and other settings
+#
+#
+#	It probably doesn't make sense to create a water class-- all of the calculations will be performed once
+#	per brew.
+#
+#	There should be as many water-related functions as possible to facilitate display
+#
+
+
